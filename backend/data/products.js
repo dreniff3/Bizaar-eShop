425f831f4615bfd07ecaa01edf11f9dc1608cd67
Don Reniff <donaldreniff@gmail.com>
2024-02-06 15:45:44 -0800

    Move into data folder in backend

---
+++ backend/data/products.js
@@ -0,0 +1,30 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Playstation 5 Console',
+    image: '/images/playstation.jpg',
+    description:
+      'The PS5 console unleashes new gaming possibilities that you never anticipated. Experience lightning fast loading with an ultra-high speed SSD, deeper immersion with support for haptic feedback, adaptive triggers, and 3D Audio*, and an all-new generation of incredible PlayStation games.',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 499.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Shin Godzilla Blu-ray',
+    image: '/images/shingodzillabluray.jpg',
+    description:
+      'From the mind behind Evangelion comes a hit larger than life. When a massive monster emerges fromt he deep and tears through the city, the government scrambles to save its citizens.',
+    brand: 'Manga Entertainment',
+    category: 'Electronics',
+    price: 18.87,
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 8,
+  },
+];
+
+export default products;
