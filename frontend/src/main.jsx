453dc1c8f29b9d1362e4a43c7974fdcf8ad5c648
Don Reniff <donaldreniff@gmail.com>
2024-02-06 14:27:04 -0800

    Add route to ProductPage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,11 +10,13 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './pages/HomePage.jsx';
+import ProductPage from './pages/ProductPage.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomePage />} />
+      <Route path="/product/:id" element={<ProductPage />} />
     </Route>
   )
 );
