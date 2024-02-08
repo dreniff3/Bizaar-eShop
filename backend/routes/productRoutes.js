11bc1714d095633d7b59a4226c59153cfbe1e549
Don Reniff <donaldreniff@gmail.com>
2024-02-08 13:53:54 -0800

    Fix routes to use controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,23 +1,10 @@
 import express from 'express';
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
+import { getProducts, getProductById } from '../controllers/productController.js';
 
 const router = express.Router();
 
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({}); // find all products
-    res.json(products);
-}));
+router.route('/').get(getProducts);
 
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id); // find 1 product
-
-    if (product) {
-        return res.json(product);
-    } else {
-        res.status(404);
-        throw new Error('Resource not found');
-    };
-}));
+router.route('/:id').get(getProductById);
 
 export default router;
