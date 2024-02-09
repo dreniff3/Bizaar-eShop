95c721697e35d2b4c4aa4fa40ff40b85de083d86
Don Reniff <donaldreniff@gmail.com>
2024-02-09 11:51:43 -0800

    Fix import

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants.js";
 
-const baseQuery = fetchBaseQuery({baseUrl: BASE_URL});
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
 // parent to other slices
 export const apiSlice = createApi({
