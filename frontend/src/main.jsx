9691315e45a8395884b86d0b2946783c5343a404
Don Reniff <donaldreniff@gmail.com>
2024-02-10 11:58:48 -0800

    Add route for CartPage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,12 +13,14 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './pages/HomePage.jsx';
 import ProductPage from './pages/ProductPage.jsx';
+import CartPage from './pages/CartPage.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomePage />} />
       <Route path="/product/:id" element={<ProductPage />} />
+      <Route path="/cart" element={<CartPage />} />
     </Route>
   )
 );
