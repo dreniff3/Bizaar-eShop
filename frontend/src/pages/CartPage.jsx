8616c0c22d992649921bf929ae60e2e300b40d1e
Don Reniff <donaldreniff@gmail.com>
2024-02-10 12:17:05 -0800

    Add checkout Card

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -84,6 +84,29 @@ const CartPage = () => {
                     </ListGroup>
                 ) }
             </Col>
+
+            {/* checkout card */}
+            <Col md={4}>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>
+                                Subtotal ({ cartItems.reduce((acc, item) => acc + item.qty, 0) }) items
+                            </h2>
+                            ${ cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2) }
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Button
+                                type='button'
+                                className='btn-block'
+                                disabled={ cartItems.length === 0 }
+                            >
+                                Proceed To Checkout
+                            </Button>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
+            </Col>
         </Row>
     );
 };
