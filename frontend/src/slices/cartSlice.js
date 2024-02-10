0e66aab135ca5536578b8b6992670cabb8d015e8
Don Reniff <donaldreniff@gmail.com>
2024-02-10 12:27:02 -0800

    Add reducer for removing from cart

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -22,9 +22,15 @@ const cartSlice = createSlice({
 
             return updateCart(state);
         },
+        removeFromCart: (state, action) => {
+            // filter out item deleted from cart
+            state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+
+            return updateCart(state);
+        },
     },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
