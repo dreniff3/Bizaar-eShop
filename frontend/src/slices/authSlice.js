2f81d68a8c00a3859c4e6ccd6daf26ba1e47ba35
Don Reniff <donaldreniff@gmail.com>
2024-02-11 14:28:59 -0800

    Create slice for authentication

---
+++ frontend/src/slices/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    userInfo: localStorage.getItem('userInfo') ? 
+        JSON.parse(localStorage.getItem('userInfo')) :
+        null,
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setCredentials: (state, action) => {
+            // payload === the user
+            state.userInfo = action.payload;
+            localStorage.setItem('userInfo', 
+                JSON.stringify(action.payload)
+            );
+        }
+    }
+});
+
+export const { setCredentials } = authSlice.actions;
+
+export default authSlice.reducer;
