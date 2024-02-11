9f71b6e13161b28ec348b4057cad2b5a0be1e2e2
Don Reniff <donaldreniff@gmail.com>
2024-02-11 13:45:06 -0800

    Add code to hash password before it is saved

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,6 +28,19 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// pre === run this code before 'save'
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        // do nothing if password was not modified
+        next();
+    }
+
+    // otherwise, hash password
+    const salt = await bcrypt.genSalt(10); // random data added to a password before it is hashed
+    // trade user password with hashed password BEFORE it is saved
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = new mongoose.model("User", userSchema);
 
 export default User;
