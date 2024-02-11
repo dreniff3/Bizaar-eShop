9f9a4056819ef249cee31c6ea353daa7963ce7fd
Don Reniff <donaldreniff@gmail.com>
2024-02-11 14:16:20 -0800

    Add code for getUserProfile and updateUserProfile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -16,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
         // generate JSON web token
         generateToken(res, user._id);
 
-        res.json({
+        res.status(200).json({
             _id: user._id,
             name: user.name,
             email: user.email,
@@ -79,14 +79,48 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @accesss Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('get user profile');
+    const user = await User.findById(req.user._id);
+    
+    if (user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 // @desc    Update user profile
 // @route   PUT /api/users/profile
 // @accesss Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.send('update user profile');
+    const user = await User.findById(req.user._id);
+    
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        // only change password if updated, since it is hashed
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updateUser.name,
+            email: updateUser.email,
+            isAdmin: updateUser.isAdmin,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 // @desc    Get users
