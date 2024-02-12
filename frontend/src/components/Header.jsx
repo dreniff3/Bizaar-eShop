f1e2f12de7dfc1ed3ebf39c85c1b04589201bc56
Don Reniff <donaldreniff@gmail.com>
2024-02-12 14:49:27 -0800

    Bugfix: destructure mutation hook as array

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const { logoutApiCall } = useLogoutMutation();
+    const [ logoutApiCall ] = useLogoutMutation();
 
     const logoutHandler = async () => {
         try {
