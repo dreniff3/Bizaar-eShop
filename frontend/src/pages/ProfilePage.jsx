92797e41e662401a0b1768aa64d3e4717d9b4293
Don Reniff <donaldreniff@gmail.com>
2024-02-19 14:50:10 -0800

    Add inputs for password to Profile page form

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -57,6 +57,26 @@ const ProfilePage = () => {
                             onChange={(e) => setEmail(e.target.value)}
                         ></Form.Control>
                     </Form.Group>
+
+                    <Form.Group controlId='password' className='my-2'>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type='password'
+                            placeholder='Enter password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        ></Form.Control>
+                    </Form.Group>
+
+                    <Form.Group controlId='confirmPassword' className='my-2'>
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control
+                            type='password'
+                            placeholder='Confirm password'
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        ></Form.Control>
+                    </Form.Group>
                 </Form>
             </Col>
             <Col md={9}>Column</Col>
