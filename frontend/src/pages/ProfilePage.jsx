cc555b570bab1c707866b6abc427530bb51cac9d
Don Reniff <donaldreniff@gmail.com>
2024-02-19 14:36:07 -0800

    Import everything Profile page needs to render

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,3 +1,13 @@
+import { useState, useEffect } from "react";
+import { Table, Form, Button, Row, Col } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { useProfileMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
+
 const ProfilePage = () => {
     return (
         <div>ProfilePage</div>
