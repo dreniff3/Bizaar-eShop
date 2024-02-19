712fc7944dc9ce85428a612729438bb6eb232116
Don Reniff <donaldreniff@gmail.com>
2024-02-19 14:33:24 -0800

    Add route for Profile page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -22,6 +22,7 @@ import PrivateRoute from './components/PrivateRoute.jsx';
 import PaymentPage from './pages/PaymentPage.jsx';
 import PlaceOrderPage from './pages/PlaceOrderPage.jsx';
 import OrderPage from './pages/OrderPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -37,6 +38,7 @@ const router = createBrowserRouter(
         <Route path="/payment" element={<PaymentPage />} />
         <Route path="/placeorder" element={<PlaceOrderPage />} />
         <Route path="/order/:id" element={<OrderPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Route>
     </Route>
   )
