fc5f43f5bc9cf82bfb142b9d2a5c9442a9d6a1cd
Don Reniff <donaldreniff@gmail.com>
2024-02-19 14:40:17 -0800

    Add functions for Profile page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -9,6 +9,29 @@ import { useProfileMutation } from "../slices/usersApiSlice";
 import { setCredentials } from "../slices/authSlice";
 
 const ProfilePage = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    
+    const dispatch = useDispatch();
+    
+    // get user info from global state
+    const userInfo = useSelector((state) => state.auth);
+
+    // get profil name and email from user info 
+    useEffect(() => {
+        if (userInfo) {
+            setName(userInfo.name);
+            setEmail(userInfo.email);
+        }
+    }, [userInfo.name, userInfo.email]);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log('Submit');
+    };
+
     return (
         <div>ProfilePage</div>
     );
