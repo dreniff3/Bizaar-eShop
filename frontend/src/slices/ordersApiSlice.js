e400222140f9e444aae4d75a8a77d5da97ce0bf2
Don Reniff <donaldreniff@gmail.com>
2024-02-19 11:28:28 -0800

    Add query for paying order

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -1,5 +1,5 @@
 import { apiSlice } from './apiSlice.js';
-import { ORDERS_URL } from '../constants.js';
+import { ORDERS_URL, PAYPALL_URL } from '../constants.js';
 
 export const ordersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
@@ -16,6 +16,13 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
                 method: 'GET',
             }),
             keepUnusedDataFor: 5, // 5 seconds
+        }),
+        payOrder: builder.mutation({
+            query: (orderId, details) => ({
+                url: `${ORDERS_URL}/${orderId}/pay`,
+                method: 'PUT',
+                body: { ...details },
+            }),
         }),
     }),
 });
