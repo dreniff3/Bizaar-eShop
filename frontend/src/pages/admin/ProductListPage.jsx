9b9d983491472f42b688770fcafeb1b48eed0f7b
Don Reniff <donaldreniff@gmail.com>
2024-02-28 13:53:23 -0800

    Add necessary imports

--- frontend/src/pages/admin/ProductListPage.jsx
+++ frontend/src/pages/admin/ProductListPage.jsx
@@ -1,4 +1,9 @@
-
+import { LinkContainer } from 'react-router-bootstrap';
+import { Table, Button, Row, Col } from 'react-bootstrap';
+import { FaTimes, FaEdit, FaTrash } from 'react-icons/fa';
+import Message from '../../components/Message';
+import Loader from '../../components/Loader';
+import { useGetProductsQuery } from '../../slices/productApiSlice';
 
 const ProductListPage = () => {
   return (
