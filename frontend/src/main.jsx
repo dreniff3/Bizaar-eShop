12905642fd6b782f4fd34db010ac060c2e4fa750
Don Reniff <donaldreniff@gmail.com>
2024-02-28 13:50:55 -0800

    Add admin route for product list page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -29,6 +29,7 @@ import PlaceOrderPage from './pages/PlaceOrderPage.jsx';
 import OrderPage from './pages/OrderPage.jsx';
 import ProfilePage from './pages/ProfilePage.jsx';
 import OrderListPage from './pages/admin/OrderListPage.jsx';
+import ProductListPage from './pages/admin/ProductListPage.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -49,6 +50,7 @@ const router = createBrowserRouter(
 
       <Route path="" element={<AdminRoute />} >
         <Route path="/admin/orderlist" element={<OrderListPage />} />
+        <Route path="/admin/productlist" element={<ProductListPage />} />
       </Route>
     </Route>
   )
