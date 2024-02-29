dcb7bd341e8affcee3394ac34cb2608923b5f28a
Don Reniff <donaldreniff@gmail.com>
2024-02-28 19:32:01 -0800

    Add form UI for editing product

--- frontend/src/pages/admin/ProductEditPage.jsx
+++ frontend/src/pages/admin/ProductEditPage.jsx
@@ -45,6 +45,81 @@ const ProductEditPage = () => {
             >
                 Go Back
             </Link>
+            <FormContainer>
+                <h1>Edit Product</h1>
+                {isUpdating && <Loader />}
+                {isLoading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
+                    <Form>
+                        <Form.Group controlId="name" className="my-2">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId="price" className="my-2">
+                            <Form.Label>Price</Form.Label>
+                            <Form.Control
+                                type="number"
+                                placeholder="Enter price"
+                                value={price}
+                                onChange={(e) => setPrice(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+
+                        {/* IMAGE INPUT PLACEHOLDER */}
+
+                        <Form.Group controlId="brand" className="my-2">
+                            <Form.Label>Brand</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter brand"
+                                value={brand}
+                                onChange={(e) => setBrand(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId="countInStock" className="my-2">
+                            <Form.Label>Count In Stock</Form.Label>
+                            <Form.Control
+                                type="number"
+                                placeholder="Enter count in stock"
+                                value={countInStock}
+                                onChange={(e) => setCountInStock(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId="category" className="my-2">
+                            <Form.Label>Category</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter category"
+                                value={category}
+                                onChange={(e) => setCategory(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId="description" className="my-2">
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter description"
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+
+                        <Button
+                            type="submit"
+                            variant="primary"
+                            className="my-2"
+                        >Update</Button>
+                    </Form>
+                )}
+            </FormContainer>
         </>
     );
 };
