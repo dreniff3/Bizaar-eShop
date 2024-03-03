35be086323085af9598d2522c1da7ab0e704fe72
Don Reniff <donaldreniff@gmail.com>
2024-03-03 12:01:12 -0800

    Edit to allow pagination

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import Message from '../components/Message.jsx';
 import { useGetProductsQuery } from '../slices/productApiSlice.js';
 
 const HomePage = () => {
-    const { data: products, isLoading, error } = useGetProductsQuery();
+    const { data, isLoading, error } = useGetProductsQuery();
 
     return (
         <>
@@ -18,7 +18,7 @@ const HomePage = () => {
             ) : (<>
                 <h1>Latest Products</h1>
                 <Row>
-                    {products.map((product) => {
+                    {data.products.map((product) => {
                         return (
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                             <Product product={product} />
