41d34ac801e2daf037d8bd44f819ba45ff924649
Don Reniff <donaldreniff@gmail.com>
2024-03-02 19:52:02 -0800

    Add form for adding product reviews

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -144,6 +144,49 @@ const ProductPage = () => {
                                 <p>{review.comment}</p>
                             </ListGroup.Item>
                         ))}
+                        <ListGroup.Item>
+                            <h2>Write a Customer Review</h2>
+                            {loadingProductReview && <Loader />}
+
+                            {userInfo ? (
+                                <Form>
+                                    <Form.Group controlId='rating' className='my-2'>
+                                        <Form.Label>Rating</Form.Label>
+                                        <Form.Control
+                                            as='select'
+                                            value={rating}
+                                            onChange={(e) => setRating(Number(e.target.value))}
+                                        >
+                                            <option value=''>Select...</option>
+                                            <option value='1'>1 - Poor</option>
+                                            <option value='2'>2 - Fair</option>
+                                            <option value='3'>3 - Good</option>
+                                            <option value='4'>4 - Very Good</option>
+                                            <option value='5'>5 - Excellent</option>
+                                        </Form.Control>
+                                    </Form.Group>
+
+                                    <Form.Group controlId='comment' className='my-2'>
+                                        <Form.Label>Comment</Form.Label>
+                                        <Form.Control
+                                            as='textarea'
+                                            row='3'
+                                            value={comment}
+                                            onChange={(e) => setComment(e.target.value)}
+                                        ></Form.Control>
+                                    </Form.Group>
+                                    <Button
+                                        disabled={loadingProductReview}
+                                        type='submit'
+                                        variant='primary'
+                                    >Submit</Button>
+                                </Form>
+                            ) : (
+                                <Message>
+                                    Please <Link to='/login'>sign in</Link> to write a review{' '}
+                                </Message>
+                            )}
+                        </ListGroup.Item>
                     </ListGroup>
                 </Col>
             </Row>
