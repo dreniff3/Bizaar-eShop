2dacd60b63296c1dffa971711ed2dad57e6baec7
Don Reniff <donaldreniff@gmail.com>
2024-03-04 14:42:24 -0800

    Add 'list' prop

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -1,7 +1,7 @@
 import { Pagination } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-const Paginate = ({ pages, page, isAdmin = false }) => {
+const Paginate = ({ list, pages, page, isAdmin = false }) => {
     return (
         pages > 1 && (
             <Pagination>
@@ -12,7 +12,7 @@ const Paginate = ({ pages, page, isAdmin = false }) => {
                             !isAdmin ? (
                                 `/page/${x + 1}`
                             ) : (
-                                `/admin/productlist/${x + 1}`
+                                `/admin/${list}/${x + 1}`
                             )
                         }
                     >
