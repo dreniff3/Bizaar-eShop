67b2a2c4728a6121b3787286836daddcb01935d7
Don Reniff <donaldreniff@gmail.com>
2024-04-15 13:26:50 -0700

    Add class name to product card link

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -17,7 +17,7 @@ const Product = ({ product }) => {
             </Link>
 
             <Card.Body>
-                <Link to={`/product/${product._id}`}>
+                <Link className="product-link" to={`/product/${product._id}`}>
                     <Card.Title as="div" className="product-title">
                         <strong>{product.name}</strong>
                     </Card.Title>
