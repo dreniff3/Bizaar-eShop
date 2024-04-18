13890eb8159c4549b9462581e370d4920fa0ba89
Don Reniff <donaldreniff@gmail.com>
2024-04-18 16:05:59 -0700

    Remove condition for checking mongoose object id

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -9,12 +9,6 @@ const errorHandler = (err, req, res, next) => {
     let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     let message = err.message;
 
-    // check for bad Mongoose ObjectId
-    if (err.name === 'CastError' && err.kind === 'ObjectId') {
-        message = 'Resource not found';
-        statusCode = 400;
-    };
-
     res.status(statusCode).json({
         message, 
         stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
